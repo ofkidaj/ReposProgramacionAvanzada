99851de3f1670bae79fd8a6da8e6c1a4e242cc7b
ofkidaj <93969210+ofkidaj@users.noreply.github.com>
2023-04-14 21:00:29 -0400

    Skynet

---
+++ IMC/AdminPersonasModel/DAL/PersonasDAL.cs
@@ -0,0 +1,55 @@
+﻿using IMC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPersonasModel.DAL
+{
+    public class PersonasDAL
+    {
+        //1. crear lista para guardar personas
+
+        private static List<Persona> personas = new List<Persona>();
+
+        //2. crear las operaciones ingresar, mostrar y buscar
+        public void AgregarPersona(Persona p)
+        {
+            personas.Add(p);
+        }
+
+        public List<Persona> ObtenerPersonas()
+        {
+            return personas;
+        }
+
+        public List<Persona> FiltrarPersonas(String nombre)
+        {
+            List<Persona> filtrada = new List<Persona>();
+
+            /* metodo 1
+
+           for (int i=0; i < personas.Count(); i++)
+             {
+                 if (personas[i].Nombre == nombre)
+                 {
+                     filtrada.Add(personas[i]);
+                 }
+           */
+            /* metodo 2
+
+            foreach (Persona persona in personas)
+            { if(p.Nombre == nombre)
+                {
+                    filtrada.Add(persona);
+                }
+            }
+            */
+            //metodo 3
+            return personas.FindAll(p => p.Nombre == nombre);
+
+            //return filtrada;
+        }
+    }
+}
---
+++ IMC/IMC/Program.cs
@@ -0,0 +1,50 @@
+﻿using IMC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    public partial class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Menu()) ;
+        }
+
+        static bool Menu()
+        {
+            bool continuar = true;
+            Console.WriteLine("1.Ingresar");
+            Console.WriteLine("2.Mostrar");
+            Console.WriteLine("3.Buscar");
+            Console.WriteLine("0.Salir");
+
+            switch (Console.ReadLine().Trim())
+            {
+                case "1":
+                    IngresarPersona();
+                    break;
+                case "2":
+                    MostrarPersona();
+                    break;
+                case "3":
+                    BuscarPersona();
+                    break;
+                case "0":
+                    continuar = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Tecla erronea");
+                    break;
+            }
+            return continuar;
+        }
+
+    }
+    }
+
+
