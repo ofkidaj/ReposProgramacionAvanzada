ff0e04ce9baae2b7c2fc2f03b719c11fcdf32889
ofkidaj <93969210+ofkidaj@users.noreply.github.com>
2023-06-20 14:33:33 -0400

    HolamundoASP

---
+++ HOLAMUNDOASP/HOLAMUNDOASP/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HOLAMUNDOASP
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        protected void Saludo_Click(object sender, EventArgs e)
+        {
+            string nombre= this.nombreTxt.Text.Trim();
+            this.mensajeh1.InnerHtml = "hola "+ nombre + ", buen dia";
+        }
+    }
+}
